eb5f1d96663a2a3731402b8a600c81c4d3efd51a
Raul Fernandez <rafeye20@hotmail.com>
2025-06-11 12:58:29 -0500

    Remove old apps folder after rename

--- apps/auth/app/db/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.orm import declarative_base
-
-
-Base = declarative_base()
-
-# Do NOT import models here to avoid circular imports.
-# Import models in your migration scripts or app startup if needed.
-
