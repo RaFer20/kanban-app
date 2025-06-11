c141111906c1cf151502b65a655ae979f14c1834
Raul Fernandez <rafeye20@hotmail.com>
2025-06-11 17:05:18 -0500

    fix: small fix in comment

--- services/auth/start.sh
+++ services/auth/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e  # Stop script on any error
 
-echo "📦 Running Alembic migrations..."
+echo "Running Alembic migrations..."
 alembic upgrade head
 
-echo "🚀 Starting FastAPI with Uvicorn..."
+echo "Starting FastAPI with Uvicorn..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
