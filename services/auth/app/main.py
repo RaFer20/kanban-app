4557234302327d741339ae801184f1015f1d29ef
Raul Fernandez <rafeye20@hotmail.com>
2025-06-12 14:38:34 -0500

    Add lifespan startup event and guest user seeding

--- services/auth/app/main.py
+++ services/auth/app/main.py
@@ -1,11 +1,22 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1 import routes
 from app.core.config import get_settings
-from fastapi.middleware.cors import CORSMiddleware
+from app.db.session import AsyncSessionLocal
+from app.seeds import create_guest_user_if_not_exists
 
 settings = get_settings()
 
-app = FastAPI(title=settings.project_name)
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Starting up the app and seeding guest user if needed...")
+    async with AsyncSessionLocal() as db:
+        await create_guest_user_if_not_exists(db)
+    yield
+    print("Shutting down the app...")
+
+app = FastAPI(title=settings.project_name, lifespan=lifespan)
 
 # Middleware for CORS
 app.add_middleware(
@@ -15,7 +26,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 app.include_router(routes.router, prefix="/api/v1")
 
